9bf6e1dfbd896a96c22d0cada19e5673acc850c5
sean1093 <sean1093@gmail.com>
2016-07-19 23:16:54 +0800

    algo.

--- search/binarysearch.js
+++ search/binarysearch.js
@@ -1,6 +1,33 @@
-(function () {
-	"use strict";
+// (function () {
+	// "use strict";
 
 
+	//key: a value
+	//target: an array
+	function binarysearch(key, target){
+		// target.sort();
+		console.log(target);
+		var left = target[0];
+		var mid = Math.round(target.length/2);
+		var right = target[target.length-1];
+		console.log("left"+left);
+		console.log("right"+right);
+		
+		while(left <= right){
+			// console.log("target[mid]"+target[mid]);
+			// console.log("key"+key);
+			if(target[mid] > key){
+				left = target[mid];
+			}
+			else if(target[mid] < key){
+				right = target[mid];
+			}
+			else if(target[mid] == key){
+				return mid;
+			}
+			// console.log("left"+left);
+			// console.log("right"+right);
+		}
+	}
 
-}());
+// }());
