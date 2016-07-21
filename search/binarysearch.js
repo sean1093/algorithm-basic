72e5213b19ed9924a12ecafc0906692f590b218e
sean1093 <sean1093@gmail.com>
2016-07-21 22:46:36 +0800

    binarysearch

--- search/binarysearch.js
+++ search/binarysearch.js
@@ -2,13 +2,13 @@
 	"use strict";
 
 	var binarysearch = {
-			/**
-			 * [search binarysearch]
-			 * @param  {[type]} key    [a key value you want to search]
-			 * @param  {[type]} target [an sorted rray]
-			 * @return {[type]}        [array index]
-			 */
-			search: function(key, target) {
+		/**
+		 * [search binarysearch]
+		 * @param  {[type]} key    [a key value you want to search]
+		 * @param  {[type]} target [an sorted array]
+		 * @return {[type]}        [array index]
+		 */
+		search: function(key, target) {
 	      	var left = 0;
 			var mid = 0;
 			var right = target.length-1;
