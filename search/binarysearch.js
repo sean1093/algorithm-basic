124e87c66c5e6493f17c9593dfa89793a5f7999c
sean1093 <sean1093@gmail.com>
2016-07-21 22:23:32 +0800

    binarysearch

--- search/binarysearch.js
+++ search/binarysearch.js
@@ -1,30 +1,36 @@
-// (function () {
-	// "use strict";
+(function () {
+	"use strict";
 
-	/**
-	 * [binarysearch description]
-	 * @param  {[type]} key    [a key value you want to search]
-	 * @param  {[type]} target [an sorted rray]
-	 * @return {[type]}        [array index]
-	 */
-	function binarysearch(key, target){
-		console.log(target);
-		var left = 0;
-		var mid = 0;
-		var right = target.length-1;
-		
-		while(left <= right){
-			mid = Math.round((left+right)/2);
-			if(target[mid] > key){
-				right = mid;
-			}
-			else if(target[mid] < key){
-				left = mid;
-			}
-			else if(target[mid] === key){
-				return mid;
+	var binarysearch = {
+			/**
+			 * [search binarysearch]
+			 * @param  {[type]} key    [a key value you want to search]
+			 * @param  {[type]} target [an sorted rray]
+			 * @return {[type]}        [array index]
+			 */
+			search: function(key, target) {
+	      	var left = 0;
+			var mid = 0;
+			var right = target.length-1;
+			
+			while(left <= right){
+				mid = Math.round((left+right)/2);
+				if(target[mid] > key){
+					right = mid;
+				}
+				else if(target[mid] < key){
+					left = mid;
+				}
+				else if(target[mid] === key){
+					return mid;
+				}
 			}
 		}
-	}
+	    
+	};
 
-// }());
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    	module.exports = binarysearch;
+ 	else
+    	window.binarysearch = binarysearch;
+})();
