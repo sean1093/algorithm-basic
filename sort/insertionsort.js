276c4783507c62520d5d466994a0e9c3ba1ced1d
sean1093 <sean1093@gmail.com>
2016-08-11 23:06:25 +0800

    insertionsort

--- sort/insertionsort.js
+++ sort/insertionsort.js
@@ -11,7 +11,12 @@
 		 */
 		sort: function(array) {
 
-			array = _insert(5,3,array);
+			for(var i=0; i<array.length; i++){
+
+				
+				// _insert(5,3,array);
+			}
+			// _insert(5,3,array);
 				
 	      	return array;
 		}
@@ -29,7 +34,6 @@
 	 * @param  {[Number]} source [source index]
 	 * @param  {[Number]} target [target index]
 	 * @param  {[Array]} array  [target array]
-	 * @return {[Array]}        [changed array]
 	 */
 	var _insert = function(source, target, array){
 		var temp = array[source];		
@@ -38,8 +42,6 @@
         	array[i + 1] = array[i];
     	}
 		array[target] = temp;
-
-		return array;
 	};
 
 
