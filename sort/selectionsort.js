fa4a2842dd6d931dfdff453df02ab53da1d6d056
sean1093 <sean1093@gmail.com>
2017-06-12 16:56:15 +0800

    selectionsort

--- sort/selectionsort.js
+++ sort/selectionsort.js
@@ -1,42 +1,43 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	var selectionsort = {
-		/**
-		 * [Selection sort]
-		 * check all elment and put the smallest to the first.
-		 * 
-		 * @param  {[type]} array [an array you want to sort]
-		 * @return {[type]}       [sorted array]
-		 */
-		sort: function(array) {
-			var min = 0;
-			var idx = 0;
-			for (var i = 0; i < array.length; i++) {
-      			idx = i;
-      			min = array[i];
+    var selectionsort = {
+        /**
+         * [Selection sort]
+         * check all elment and put the smallest to the first.
+         * 把第i筆資料插入前面(i-1)筆已經排序好的陣列中
+         * 
+         * @param  {[type]} array [an array you want to sort]
+         * @return {[type]}       [sorted array]
+         */
+        sort: function(array) {
+            var min = 0;
+            var idx = 0;
+            for (var i = 0; i < array.length; i++) {
+                idx = i;
+                min = array[i];
 
-	      		for (var j = i+1; j < array.length; j++) {
-	      			if(array[j] < min ){
-	      				min = array[j];
-	      				idx = j;
-	      			}
-	      		}
-	      		_swap(i, idx, array);
-	      	}
-	      	return array;
-		}
-	    
-	};
+                for (var j = i+1; j < array.length; j++) {
+                    if(array[j] < min ){
+                        min = array[j];
+                        idx = j;
+                    }
+                }
+                _swap(i, idx, array);
+            }
+            return array;
+        }
+        
+    };
 
-	var _swap = function(x, y, array){
-		var temp = array[y];
-		array[y] = array[x];
-		array[x] = temp;
-	};
+    var _swap = function(x, y, array){
+        var temp = array[y];
+        array[y] = array[x];
+        array[x] = temp;
+    };
 
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-    	module.exports = selectionsort;
- 	else
-    	window.selectionsort = selectionsort;
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+        module.exports = selectionsort;
+    else
+        window.selectionsort = selectionsort;
 })();
