42c0f273243e21cc4cdc00677c7a838d507b5664
sean1093 <sean1093@gmail.com>
2017-06-12 16:52:47 +0800

    bubblesort

--- sort/bubblesort.js
+++ sort/bubblesort.js
@@ -1,38 +1,33 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	var bubblesort = {
-		/**
-		 * [Bubble sort]
-		 * check two elements from 0 to n and put large one to the left.
-		 * 
-		 * @param  {[type]} array [an array you want to sort]
-		 * @return {[type]}        [sorted array]
-		 */
-		sort: function(array) {
-			var n = array.length;
+    var bubblesort = {
+        /**
+         * [Bubble sort]
+         * check two elements from 0 to n and put large one to the left.
+         * 兩兩互相比較，每回合將最大值升到最高格子，像氣泡往上升一樣
+         * 
+         * @param  {[type]} array [an array you want to sort]
+         * @return {[type]}        [sorted array]
+         */
+        sort: function(array) {
+            var n = array.length;
+            for (var i = 0; i < n-1; i++) {
+                for (var j = 0; j<n-i-1; j++) {
+                    if(array[j] > array[j+1]){
+                        var temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        
+    };
 
-			for (var i = 0; i < n-1; i++) {
-				for (var j = 0; j<n-i-1; j++) {
-					if(array[j] > array[j+1]){
-						// console.log("swap: "+array[j]+", "+array[j+1]);
-						_swap(j, j+1, array);
-					}
-				}
-			}
-	      	return array;
-		}
-	    
-	};
-
-	var _swap = function(x, y, array){
-		var temp = array[y];
-		array[y] = array[x];
-		array[x] = temp;
-	};
-
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-    	module.exports = bubblesort;
- 	else
-    	window.bubblesort = bubblesort;
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+        module.exports = bubblesort;
+    else
+        window.bubblesort = bubblesort;
 })();
