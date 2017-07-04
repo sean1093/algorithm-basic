e078d15dc1211f2dbcbb38ea829fa58fe20120fb
sean1093 <sean1093@gmail.com>
2017-07-04 22:28:49 +0800

    dev

--- sort/quicksort.js
+++ sort/quicksort.js
@@ -10,23 +10,53 @@
          * @return {[type]}        [sorted array]
          */
         sort: function(array) {
-
+            var _swap = function(x, y, array){
+                var temp = array[y];
+                array[y] = array[x];
+                array[x] = temp;
+            };
+            
             var quickSort = function(array, left, right) {
                 if(left < right) {
                     var i = left;
                     var j = right;
                     var pivot = array[i];
+                    var length = array.length;
+                    var count = 0;
                     // 先將第一個當成pivot
                     while(i<j) {
-                        從頭(left)開始找第一個大於pivot值的位置i
-                            while() {}
+                        // 從頭(left)開始找第一個大於pivot值的位置i
+                        while(i < length && i < pivot) {
+                            i++;
 
-                        從尾(right)開始找第一個小於pivot值的位置j
-                    }                   
+
+                            count++;
+                            if(count > 100){break;}
+                        }
+
+                        // 從尾(right)開始找第一個小於pivot值的位置j
+                        while(j > left && j > pivot) {
+                            j--;
+
+
+                            count++;
+                            if(count > 100){break;}
+                        }
+
+
+                        count++;
+                        if(count > 100){break;}
+
+
+                        _swap(i, j, array);
+                    }
+                    _swap(left, j, array); 
+                    quickSort(array, left, j-1);   
+                    quickSort(array, j+1, right);                  
                 }
-
-            }
-
+                
+            };
+            quickSort(array, 0, array.length-1);
 
             return array;
         }
